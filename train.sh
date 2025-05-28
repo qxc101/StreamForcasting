df61dc28ab221029bce7f1c7907317904f162086
qxc101 <qxc101@case.edu>
2025-05-28 11:24:40 -0400

    cl script

---
+++ train.sh
@@ -0,0 +1,8 @@
+# hourly 24 steps
+python FutureTST_train_eval_hourly_skip_batch.py --time_series_size 144 --d_model 128 --embedding_dropout 0.0 --mlp_dropout 0.1 --mlp_size 128 --num_heads 16 --num_transformer_layers 2 --patch_size 32 --pred_size 24 --stride_len 16
+
+# hourly 48 steps
+python FutureTST_train_eval_hourly_skip_batch.py --time_series_size 144 --d_model 128 --embedding_dropout 0.1 --mlp_dropout 0.1 --mlp_size 256 --num_heads 8 --num_transformer_layers 2 --patch_size 32 --pred_size 48 --stride_len 16
+
+# 6-hourly 28 steps
+python FutureTST_train_eval_hourly_skip_batch.py --time_series_size 144 --d_model 128 --embedding_dropout 0.0 --mlp_dropout 0.1 --mlp_size 128 --num_heads 16 --num_transformer_layers 2 --patch_size 32 --pred_size 28 --stride_len 16 --sixhourly
